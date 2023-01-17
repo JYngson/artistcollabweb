4bd9e0a4272500d7290732a31dcfa9e52089a491
tjarevalo <jimmyjamesyngson@gmail.com>
2023-01-16 16:33:52 -0800

    completed auth cycle

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,64 @@
-"use client"
+'use client'
 //Change use client later when creating login route
+import { useEffect, useState } from 'react';
 
 export default function HomePage() {
   const axios = require('axios');
+  const currentURL = window.location.search;
+  const searchParams = new URLSearchParams(currentURL);
+
+  let [code, setCode] = useState<String | null>()
 
   const redirect = (e:React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault()
-    axios.get('http://localhost:8080/auth')
+    axios.get('http://localhost:8080/token')
     .then((res:any) => {
-      window.location.assign(res.request.responseURL)
+      window.location.assign(res.request.responseURL);
     })
     .catch((err:any) => {
-      console.log(err)
+      console.log(err);
     })
   }
 
+  function authCheck(){
+    if (searchParams.has('code')){
+      let getCode = searchParams.get('code');
+      setCode(getCode);
+    }
+
+    if (searchParams.has('error')){
+      let error = searchParams.get('error');
+      console.log(error);
+    }
+  }
+
+
+  function authentication(){
+    axios.get('/auth',{
+      grant_type: 'authorization_code',
+      code: code,
+      redirect_uri: 'http://localhost:3000',
+    })
+    .then((res:any) => {
+      console.log(res);
+    })
+    .catch((err:any) => {
+      console.log(err);
+    })
+  }
+
+  useEffect(() => {
+    authCheck();
+
+    if (code){
+      authentication();
+    }
+
+  }, [])
+
   return (
-    <div id="HomePage" className='w-screen h-screen bg-zinc-300'>
-      <div id="Card" className="w-1/6 h-3/6 flex flex-col justify-center items-center bg-blue-300">
+    <div id='HomePage' className='w-screen h-screen bg-zinc-300'>
+      <div id='Card' className='w-1/6 h-3/6 flex flex-col justify-center items-center bg-blue-300'>
         <h1>Card</h1>
         <h2>Artist Name</h2>
         <p>Albums</p>
--- pages/api/server.js
+++ pages/api/server.js
@@ -1,25 +1,110 @@
 // server.js
-require('dotenv').config()
-const express = require('express')
+require('dotenv').config();
+const express = require('express');
+const axios = require('axios')
 const app = express();
 const querystring = require('querystring');
 
-app.get('/' , function (req, res){
+let redirect_uri = 'http://localhost:8080/auth';
+let clientID = process.env.CLIENT_ID.toString();
+let clientSecret = process.env.CLIENT_SECRET.toString();
+
+app.get('/' , (req, res) =>{
   res.send('listening on port 8080')
 })
 
-app.get('/auth', function(req, res) {
-  let clientID = 'a4a59d570aa44c9c9c6448f19d72beb5'
-  let redirect_uri = 'http://localhost:3000';
-  
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:8080"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+function randomStringGen(length) {
+  let result = '';
+  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let charactersLength = characters.length;
+  for ( let i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+let stateKey = 'spotify_auth_key'
+
+app.get('/token',(req, res) => {
+  const state = randomStringGen(16)
+  res.cookie(stateKey, state)
+
   res.redirect('https://accounts.spotify.com/authorize?' +
     querystring.stringify({
       response_type: 'code',
       client_id: clientID,
       redirect_uri: redirect_uri,
+      state: state
   }));
 });
 
+app.get('/auth',(req, res) => {
+  let code = req.query.code || null;
+
+  axios({
+    method: 'post',
+    url: 'https://accounts.spotify.com/api/token',
+    data:
+      querystring.stringify({
+        grant_type: "authorization_code",
+        code: code,
+        redirect_uri: redirect_uri,
+      }),
+    headers: {
+      "Authorization": `Basic ${new Buffer.from(`${clientID}:${clientSecret}`).toString('base64')}`,
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+  })
+    .then(response => {
+      if (response.status == 200){
+        const { access_token, token_type } = response.data;
+        const { refresh_token } = response.data
+
+        axios.get(`http://localhost:8080/tokenRefresh?refresh_token=${refresh_token}`)
+          .then(response => {
+            res.send(`<pre>${JSON.stringify(response.data, null, 2)}</pre>`)
+          })
+          .catch(err => {
+            res.send(err)
+          })
+      } else {
+        res.send(response)
+      }
+    })
+    .catch(err => {
+      res.send(err)
+    })
+})
+
+app.get('/tokenRefresh', (req, res) => {
+  const {refresh_token} = req.query
+
+  axios({
+    method: 'post',
+    url: 'https://accounts.spotify.com/api/token',
+    data:
+      querystring.stringify({
+        grant_type: "refresh_token",
+        refresh_token: refresh_token
+      }),
+    headers: {
+      "Authorization": `Basic ${new Buffer.from(`${clientID}:${clientSecret}`).toString('base64')}`,
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+  })
+  .then(response => {
+    res.send(`${response} Refresh Endpoint Successful`)
+  })
+  .catch(error => {
+    res.send(error)
+  })
+})
 
 app.listen(8080, (err) => {
   if (err) throw err
