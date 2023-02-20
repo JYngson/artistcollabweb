62bfc784e45c1187ef0203999cb0ede4f306ff16
tjarevalo <jimmyjamesyngson@gmail.com>
2023-02-19 18:41:13 -0800

    testing conditional rendering of multiple coins

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -5,14 +5,32 @@ import { TextureLoader } from 'three/src/loaders/TextureLoader'
 import { Suspense } from "react";
 import { Physics, useCylinder, usePlane } from '@react-three/cannon'
 
-const testData = [{artistName: 'Gallant', collabCount: 2},
-{artistName: 'Kendrick Lamar', collabCount: 5},
-{artistName: 'Jey Lato', collabCount: 3},
-{artistName: 'Ed Sheeran', collabCount: 9},
-{artistName: 'GUY', collabCount: 6},
-{artistName: 'Buffy', collabCount: 8},
-{artistName: 'Giles', collabCount: 4}]
+const testData = [
+  {artistName: 'Gallant', collabCount: 2},
+  {artistName: 'Kendrick Lamar', collabCount: 5},
+  {artistName: 'Jey Lato', collabCount: 3},
+  {artistName: 'Ed Sheeran', collabCount: 9},
+  {artistName: 'GUY', collabCount: 6},
+  {artistName: 'Buffy', collabCount: 8},
+  {artistName: 'Giles', collabCount: 4},
+  {artistName: 'Cordelia', collabCount: 7},
+  {artistName: 'Kenny G', collabCount: 1},
+  {artistName: 'Reikland', collabCount: 3},
+  {artistName: 'Lokhir Fellheart', collabCount: 5},
+  {artistName: 'Sandy', collabCount: 10},
+  {artistName: 'Thea', collabCount: 9},
+  {artistName: 'Justin Bieber', collabCount: 2},
+  {artistName: 'Drake', collabCount: 1},
+  {artistName: 'Journey', collabCount: 5},
+  {artistName: 'Edro', collabCount: 4},
+  {artistName: 'YourMom', collabCount: 69},
+] // 18 tests
 
+const collabSum = testData.reduce(function(acc, collaborator){
+  return acc + collaborator.collabCount
+}, 0)
+
+console.log(collabSum)
 
 //React Fiber && THREE.js
 function Cylinder({position, rotation}){
@@ -28,10 +46,8 @@ function Cylinder({position, rotation}){
   //Radius, Height, Width
   return (
     <mesh ref={ref}>
-      <cylinderGeometry args={[2,2,0.5]} />
+      <cylinderGeometry args={[3,3,0.5]} />
       <meshStandardMaterial attach='material' color='#FFA500' map={artistImage} />
-      <Text position={[3,0,0]} color='red' rotation={[Math.PI / 2, 0 , Math.PI / 2]}>Artist Names</Text>
-      <Text position={[4,0,0]} color='blue' rotation={[Math.PI / 2, 0 , Math.PI / 2]}>Collaborations: 2</Text>
     </mesh>
   )
 }
@@ -47,15 +63,32 @@ function Plane({position, rotation}){
   ));
   return(
     <mesh ref={ref}>
-      <planeGeometry args={[30,30]} />
+      <planeGeometry args={[10000,10000]} />
       <meshStandardMaterial attach='material' color ='#FFFFFF'/>
     </mesh>
   )
 }
 
-
+function CollabCoin({position, rotation, size}){
+  const artistImage = useLoader(TextureLoader,'/test.png')
+  const [ref] = useCylinder(() => (
+    {
+      mass: 10,
+      position: position,
+      rotation: rotation
+    }
+  ))
+  //Radius, Height, Width
+  return (
+    <mesh ref={ref}>
+      <cylinderGeometry args={[size,size,0.5]} />
+      <meshStandardMaterial attach='material' color='#FFA500' map={artistImage} />
+    </mesh>
+  )
+}
 
 export default function page() {
+  console.log(testData)
   return (
     <div id='canvas-container' className='w-screen h-screen'>
       <Canvas camera={{ fov: 75, near: 0.1, far: 1000, position: [0,10,0] }}>
@@ -69,6 +102,24 @@ export default function page() {
           <Physics>
             <Plane position={[0,0,0]} rotation={[-Math.PI / 2, 0, 0 ]} />
             <Cylinder position={[0,1,0]} rotation={[Math.PI, Math.PI / 2, 0]}/>
+          {
+            testData.map((collaborator, index) => {
+              let coinSize:number = Math.round((collaborator.collabCount / collabSum) * testData.length) + 1
+              let zPosition = index % 2 ? 6 : -6
+              function xPositionCalc(index) {
+                if(index % 2 == 0){
+                  return index % 4 == 0 ? 4 : -4
+                } else {
+                  return index % 3 == 0 ? 4 : -4
+                }
+              }
+              let xPosition = xPositionCalc(index)
+
+              return(
+                <CollabCoin key={index} position={[xPosition,10,zPosition]} rotation={[Math.PI, Math.PI / 2, 0]} size={coinSize} />
+              )
+            })
+          }
           </Physics>
         </Suspense>
       </Canvas>
