46e545dbb70ac1a96c26ada2fc73115f6b26cad8
tjarevalo <jimmyjamesyngson@gmail.com>
2023-02-19 16:03:17 -0800

    added cannon-es

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -3,6 +3,7 @@ import { Canvas, useLoader } from '@react-three/fiber'
 import { OrbitControls, Text } from '@react-three/drei'
 import { TextureLoader } from 'three/src/loaders/TextureLoader'
 import { Suspense } from "react";
+import * as CANNON from 'cannon-es'
 
 const testData = [{artistName: 'Gallant', collabCount: 2},
 {artistName: 'Kendrick Lamar', collabCount: 5},
@@ -13,6 +14,7 @@ const testData = [{artistName: 'Gallant', collabCount: 2},
 {artistName: 'Giles', collabCount: 4}]
 
 
+//React Fiber && THREE.js
 function Cylinder(props){
   const artistImage = useLoader(TextureLoader,'/test.png')
   //Radius, Height, Width
@@ -36,19 +38,32 @@ function Plane(props){
   )
 }
 
+//Cannon-es code
+const world = new CANNON.World({
+  gravity: new CANNON.Vec3(0, -9.82, 0), // m/s²
+})
+
+const groundBody = new CANNON.Body({
+  type: CANNON.Body.STATIC,
+  shape: new CANNON.Plane(),
+})
+groundBody.quaternion.setFromEuler(-Math.PI / 2, 0, 0)
+
+world.addBody(groundBody)
 
 export default function page() {
   return (
     <div id='canvas-container' className='w-screen h-screen'>
-      <Canvas camera={{ fov: 75, near: 0.1, far: 1000, position: [0, 0, 15] }}>
+      <Canvas camera={{ fov: 75, near: 0.1, far: 1000, position: [0,10,0] }}>
         <color attach="background" args={['#1e293b']} />
-        <ambientLight intensity={0.5}/>
-        <spotLight color='white' intensity={0.5} position={[0,0,5]} />
-        {/* <PerspectiveCamera /> */}
+        <ambientLight intensity={1}/>
         <OrbitControls />
         <Suspense fallback={null}>
-          <Plane position={[0,0,0]} />
-          <Cylinder position={[0,0,1]} rotation={[-Math.PI / 2, Math.PI / 2, 0]}/>
+          <gridHelper args={[20,20, '#FF0000', '#FF0000']} />
+          <gridHelper args={[20,20, '#00FF00', '#00FF00']} rotation={[0,0,Math.PI / 2]} />
+          <gridHelper args={[20,20, '#0000FF', '#0000FF']} rotation={[Math.PI / 2,0,0]} />
+          <Plane position={[0,0,0]} rotation={[-Math.PI / 2, 0, 0 ]} />
+          {/* <Cylinder position={[0,1,0]} rotation={[Math.PI, Math.PI / 2, 0]}/> */}
         </Suspense>
       </Canvas>
     </div>
